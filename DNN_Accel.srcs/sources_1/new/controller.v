5494ceecf97254f018a40d757aa207b16ef09a63
be <beoppchaser@gmail.com>
2025-12-14 18:18:15 +0800

    perf: replace some register (like kernel_size) with constants, to simplify synthesis

--- DNN_Accel.srcs/sources_1/new/controller.v
+++ DNN_Accel.srcs/sources_1/new/controller.v
@@ -23,6 +23,8 @@
 module controller (
     input wire clk,
     input wire reset,
+    // these 3 lines are just for convenient testing
+    // will consume lots of time in synthesis
     input wire [7:0] filter_buffer [127:0][4:0],
     input wire [7:0] input_image [31:0][31:0],
     output reg [7:0] output_buffer [7:0][31:0][31:0]
@@ -35,21 +37,17 @@ module controller (
     // PE tile size
     localparam MAX_PE_TILE_HEIGHT = 5;
     localparam MAX_PE_TILE_WIDTH = 16;
-
-    reg [5:0] pe_tile_height;
-    reg [5:0] pe_tile_width;
+    localparam PE_TILE_HEIGHT = 5;
+    localparam PE_TILE_WIDTH = 16;
 
     // same value for all PEs
-    reg kernel_type;
-    reg stride;
-    reg [3:0] kernel_size;
-    reg [5:0] ifm_size;
+    localparam KERNEL_TYPE = 1;
+    localparam STRIDE_TYPE = 0;
+
+    localparam IFM_SIZE = 32;
 
     // start signals for each row of a PE tile
     reg start [MAX_PE_TILE_HEIGHT-1:0];
-
-    // use previous psum signals for each row of a PE tile
-    reg use_prev_psum [MAX_PE_TILE_HEIGHT-1:0];
 
     // original layout
     // reg [7:0] input_image [31:0][31:0];
@@ -97,12 +95,12 @@ module controller (
 
                 PE pe_inst (
                     .clk(clk),
-                    .kernel_type(kernel_type),
-                    .start(start[i % pe_tile_height]),
-                    .use_prev_psum(use_prev_psum[i % pe_tile_height]),
+                    .kernel_type(KERNEL_TYPE),
+                    .start(start[i % PE_TILE_HEIGHT]),
+                    .use_prev_psum(i % (KERNEL_TYPE ? 5 : 3) == 0 ? 0 : 1),
                     .filter_data(pe_filter_data[i][j/2]),
-                    .ifm_data1(pe_ifm_data1[i % pe_tile_height][j % pe_tile_width]),
-                    .ifm_data2(pe_ifm_data2[i % pe_tile_height][j % pe_tile_width]),
+                    .ifm_data1(pe_ifm_data1[i % PE_TILE_HEIGHT][j % PE_TILE_WIDTH]),
+                    .ifm_data2(pe_ifm_data2[i % PE_TILE_HEIGHT][j % PE_TILE_WIDTH]),
                     .psum_data1(psum_in1[i][j]),
                     .psum_data2(psum_in2[i][j]),
                     .psum_out1(psum_out1[i][j]),
@@ -112,26 +110,13 @@ module controller (
         end
     endgenerate
 
-    // used to test conv1
-    always @(posedge clk) begin
-        kernel_type <= 1;
-        stride <= 0;
-        kernel_size <= 5;      // actually it's always equal to pe_tile_height
-        ifm_size <= 32;
-        pe_tile_height <= MAX_PE_TILE_HEIGHT;
-        pe_tile_width <= MAX_PE_TILE_WIDTH;
-        for (integer i = 0; i < HEIGHT; i = i + 1) begin
-            use_prev_psum[i] <= (i % 5 == 0 ? 0 : 1);
-        end
-    end
-
     function [7:0] img_after_padding;
         input integer r;
         input integer c;
 
         begin
-            automatic integer m = (kernel_size - 1) / 2;
-            automatic integer M = ifm_size + m - 1;
+            automatic integer m = KERNEL_TYPE ? 2 : 1;
+            automatic integer M = IFM_SIZE + m - 1;
             if (r < m || r > M || c < m || c > M) begin
                 img_after_padding = 0;
             end else begin
@@ -152,21 +137,21 @@ module controller (
             cycle_count <= cycle_count + 1;
 
             // load ifm and filter data to PEs at the right time
-            if ((cycle_count + 1) % kernel_size == 0) begin
+            if ((cycle_count + 1) % (KERNEL_TYPE ? 5 : 3) == 0) begin
                 load_count <= load_count + 1;
 
                 // set pe ifm data according to original image
                 for (integer i = 0; i < MAX_PE_TILE_HEIGHT; i = i + 1) begin
                     for (integer j = 0; j < MAX_PE_TILE_WIDTH; j = j + 1) begin
-                        if (i == 0 || j == pe_tile_width-1) begin
+                        if (i == 0 || j == PE_TILE_WIDTH-1) begin
                             automatic integer r1 = i + j;
-                            automatic integer r2 = r1 + pe_tile_width;
+                            automatic integer r2 = r1 + PE_TILE_WIDTH;
                             automatic integer c = load_count - i;
                             for (integer k = 0; k < 5; k = k + 1) begin
                                 pe_ifm_data1[i][j][k] <= img_after_padding(r1, c + k);
                                 pe_ifm_data2[i][j][k] <= img_after_padding(r2, c + k);
                             end
-                        end else begin
+                        end else if (i < PE_TILE_HEIGHT && j < PE_TILE_WIDTH) begin
                             pe_ifm_data1[i][j] <= pe_ifm_data1[i-1][j+1];
                             pe_ifm_data2[i][j] <= pe_ifm_data2[i-1][j+1];
                         end
@@ -174,12 +159,12 @@ module controller (
                 end
 
                 // set pe filter data according to filter buffer
-                if (load_count < kernel_size) begin
+                if (load_count < (KERNEL_TYPE ? 5 : 3)) begin
                     for (integer i = 0; i < HEIGHT; i = i + 1) begin
                         for (integer j = 0; j < WIDTH; j = j + 2) begin
-                            automatic integer index = i / kernel_size * (WIDTH / pe_tile_width)
-                                                    + j / pe_tile_width;
-                            if (i % kernel_size == load_count % kernel_size) begin
+                            automatic integer index = i / (KERNEL_TYPE ? 5 : 3) * (WIDTH / PE_TILE_WIDTH)
+                                                    + j / PE_TILE_WIDTH;
+                            if (i % (KERNEL_TYPE ? 5 : 3) == load_count % (KERNEL_TYPE ? 5 : 3)) begin
                                 pe_filter_data[i][j/2] <= filter_buffer[index];
                             end
                         end
@@ -188,19 +173,19 @@ module controller (
             end
 
             // store output psum to output buffer at the right time
-            if (cycle_count > (kernel_size - 1 + 29) && (cycle_count - (kernel_size - 1 + 29)) % 5 == 1) begin
+            if (cycle_count > ((KERNEL_TYPE ? 5 : 3) - 1 + 29) && (cycle_count - ((KERNEL_TYPE ? 5 : 3) - 1 + 29)) % 5 == 1) begin
                 store_count <= store_count + 1;
 
                 for (integer i = 0; i < HEIGHT; i = i + 1) begin
-                    if ((i + 1) % kernel_size == 0) begin
+                    if ((i + 1) % (KERNEL_TYPE ? 5 : 3) == 0) begin
                         for (integer j = 0; j < WIDTH; j = j + 1) begin
-                            automatic integer img_r = i / kernel_size;
-                            automatic integer img_c = j / pe_tile_width;
-                            automatic integer img_index = img_r * (WIDTH / pe_tile_width) + img_c;
-                            automatic integer r = j % pe_tile_width;
+                            automatic integer img_r = i / (KERNEL_TYPE ? 5 : 3);
+                            automatic integer img_c = j / PE_TILE_WIDTH;
+                            automatic integer img_index = img_r * (WIDTH / PE_TILE_WIDTH) + img_c;
+                            automatic integer r = j % PE_TILE_WIDTH;
 
                             output_buffer[img_index][r][store_count] <= psum_out1[i][j];
-                            output_buffer[img_index][r + pe_tile_width][store_count] <= psum_out2[i][j];
+                            output_buffer[img_index][r + PE_TILE_WIDTH][store_count] <= psum_out2[i][j];
                         end
                     end
                 end
@@ -208,7 +193,7 @@ module controller (
 
             // generate start signals
             for (integer i = 0; i < MAX_PE_TILE_HEIGHT; i = i + 1) begin
-                if (cycle_count == (i + 1) * kernel_size - 1) begin
+                if (cycle_count == (i + 1) * (KERNEL_TYPE ? 5 : 3) - 1) begin
                     start[i] <= 1;
                 end else begin
                     start[i] <= 0;
